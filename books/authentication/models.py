43b6b78fb74e1cae6edeec3d41ac4e3fe96b3679
expensive77 <aligeranmayeh9797@gmail.com>
2021-10-24 11:57:58 +0330

    add user model to settings.py

--- books/authentication/models.py
+++ books/authentication/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=300)
     username = models.CharField(max_length=300, unique=True)
@@ -10,5 +11,3 @@ class CustomUser(AbstractUser):
     address = models.TextField()
     is_private_person = models.BooleanField(default=False)
     is_book_store = models.BooleanField(default=False)
-
-
