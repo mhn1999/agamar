f5b0f180b1685e03c004c8675a5e2c583255aff7
expensive77 <aligeranmayeh9797@gmail.com>
2021-10-24 15:23:31 +0330

    updeta serializer fields and remove being null in phone_number

--- books/authentication/serializer.py
+++ books/authentication/serializer.py
@@ -5,7 +5,8 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'name', 'phone_number', 'address',
+                  'is_private_person', 'is_book_store']
         extra_kwargs = {
             'password': {'write_only': True}
         }
