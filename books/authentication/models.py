9258a7885353933785c507d85ae90933cf4a68b6
expensive77 <aligeranmayeh9797@gmail.com>
2021-10-24 13:16:01 +0330

    add null value for phone number and run a test

--- books/authentication/models.py
+++ books/authentication/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
     username = models.CharField(max_length=300, unique=True)
     email = models.CharField(max_length=400, unique=True)
     password = models.CharField(max_length=100)
-    phone_number = models.IntegerField(unique=True)
+    phone_number = models.IntegerField(unique=True, null=True)
     address = models.TextField()
     is_private_person = models.BooleanField(default=False)
     is_book_store = models.BooleanField(default=False)
