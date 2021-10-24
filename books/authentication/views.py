c9e36b5b5dac44e23609e1ec97b1796d1d3db0e3
expensive77 <aligeranmayeh9797@gmail.com>
2021-10-24 12:42:36 +0330

    add serializer to RegisterView

--- books/authentication/views.py
+++ books/authentication/views.py
@@ -1,5 +1,10 @@
 from rest_framework.views import APIView
+from .serializer import CustomUserSerializer
+from rest_framework.response import Response
 
 class RegisterView(APIView):
     def post(self,request):
-        pass
+        serializer = CustomUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message": serializer.data})
