e2a39ec5ea445cd6ee863eaf674a6d170ffbfce5
expensive77 <aligeranmayeh9797@gmail.com>
2021-10-24 15:39:28 +0330

    add url for login

--- books/authentication/urls.py
+++ books/authentication/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('register', RegisterView.as_view())
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view())
+
 ]
