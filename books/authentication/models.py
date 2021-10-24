be8c24efe09f4e95e7c699efdf2ff8755e838e7a
expensive77 <aligeranmayeh9797@gmail.com>
2021-10-24 11:53:40 +0330

    create user model

--- books/authentication/models.py
+++ books/authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=300)
+    username = models.CharField(max_length=300, unique=True)
+    email = models.CharField(max_length=400, unique=True)
+    password = models.CharField(max_length=100)
+    phone_number = models.IntegerField(unique=True)
+    address = models.TextField()
+    is_private_person = models.BooleanField(default=False)
+    is_book_store = models.BooleanField(default=False)
+
+
