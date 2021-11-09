383698132b72f19c15e5ffd1a18ad60fcc9baebb
expensive77 <aligeranmayeh9797@gmail.com>
2021-11-09 18:33:16 +0330

    initial serializer for update password

--- books/authentication/serializer.py
+++ books/authentication/serializer.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from rest_framework import serializers
 from .models import CustomUser
@@ -61,3 +62,28 @@ class UserUpdateInfoSerializer(serializers.ModelSerializer):
         def update(self, validated_data):
             instance = self.Meta.model(**validated_data)
             return instance
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    old_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('old_password', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError({"old_password": "Old password is not correct"})
+        return value
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
