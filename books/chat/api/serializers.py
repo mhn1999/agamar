705f76eaee9c9a17c903b01609f8590c7175fee7
expensive77 <aligeranmayeh9797@gmail.com>
2021-11-29 19:09:40 +0330

    add chat serializer for view

---
+++ books/chat/api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from chat.models import Chat, Contact
+from chat.views import get_user_contact
+
+
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    participants = ContactSerializer(many=True)
+
+    class Meta:
+        model = Chat
+        fields = ('id', 'messages', 'participants')
+        read_only = ('id')
+
+    def create(self, validated_data):
+        print(validated_data)
+        participants = validated_data.pop('participants')
+        chat = Chat()
+        chat.save()
+        for username in participants:
+            contact = get_user_contact(username)
+            chat.participants.add(contact)
+        chat.save()
+        return chat
