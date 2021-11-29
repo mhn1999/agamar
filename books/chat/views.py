59883036d4db6179f89c3b2d1505fbb5337d4866
mak1047 <mohsenakbari1378@gmail.com>
2021-11-29 18:24:29 +0330

    putting all togather

--- books/chat/views.py
+++ books/chat/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404
+from .models import Chat, Contact
 
-# chat/views.py
-from django.shortcuts import render
+User = get_user_model()
 
-def index(request):
-    return render(request, 'chat/index.html')
 
-def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name})
+def get_last_10_messages(chatId):
+    chat = get_object_or_404(Chat, id=chatId)
+    return chat.messages.order_by('-timestamp').all()[:10]
+
+
+def get_user_contact(username):
+    user = get_object_or_404(User, username=username)
+    return get_object_or_404(Contact, user=user)
+
+
+def get_current_chat(chatId):
+    return get_object_or_404(Chat, id=chatId)
